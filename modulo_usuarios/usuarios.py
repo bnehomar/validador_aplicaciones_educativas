a282d28bc7bebaa364f673254396c094f7ede443
Yoel Jerez <yoel.jerez@gmail.com>
2015-07-30 00:21:12 -0430

    modulo de modificar usuario listo

--- modulo_usuarios/usuarios.py
+++ modulo_usuarios/usuarios.py
@@ -79,6 +79,28 @@ class Proceso_usuarios():
 
         #self.boton_aceptar_incluir_usuario = self.constructor.get_object("boton_aceptar_incluir_usuario")
 
+        self.window_cedula_modificar_usuario = self.constructor.get_object("window_cedula_modificar_usuario")
+        self.entry_cedula_modificar = self.constructor.get_object("entry_cedula_modificar")
+
+        self.window_cedula_no_existe = self.constructor.get_object("window_cedula_no_existe")
+
+        #Modificar usuario
+        self.window_modificar_usuario = self.constructor.get_object("window_modificar_usuario")
+        #Datos del usuario
+        self.entry_modificar_nombre = self.constructor.get_object("entry_modificar_nombre")
+        self.entry_modificar_apellido = self.constructor.get_object("entry_modificar_apellido")
+        self.entry_modificar_cedula = self.constructor.get_object("entry_modificar_cedula")
+        self.entry_modificar_clave = self.constructor.get_object("entry_modificar_clave")
+
+        # the combobox
+        self.combobox_activar_modificar_usuario = self.constructor.get_object("combobox_activar_modificar_usuario")
+        self.combobox_activar_modificar_usuario.set_model(self.liststore_usuario_activo)
+        self.cell = Gtk.CellRendererText()
+        self.combobox_activar_modificar_usuario.pack_start(self.cell, True)
+        self.combobox_activar_modificar_usuario.add_attribute(self.cell, 'text', 1)
+
+        self.window_usuario_modificado = self.constructor.get_object("window_usuario_modificado")
+
         self.window_menu_usuario.show()
 
     def on_combobox_activar_usuario_changed(self, widget, data=None):
@@ -193,6 +215,143 @@ class Proceso_usuarios():
     def on_boton_aceptar_errores_incluir_usuario_clicked(self, widget):
     	self.window_errores_incluir_usuario.hide()
 
+    def on_boton_modificar_usuario_clicked(self, widget):
+    	self.entry_cedula_modificar.set_text("")
+    	self.window_cedula_modificar_usuario.show()
+
+    def on_button_aceptar_cedula_no_existe_clicked(self, widget):
+    	self.window_cedula_no_existe.hide()
+    	self.entry_cedula_modificar.set_text("")
+
+    def on_button_aceptar_cedula_modificar_clicked(self, widget):
+    	cedula = self.entry_cedula_modificar.get_text()
+    	
+    	usuario = self.get_cedula(cedula)
+
+    	if len(usuario) > 0:
+    		self.set_modificar_usuario(usuario)
+    		self.window_cedula_modificar_usuario.hide()
+    	else:
+    		self.window_cedula_no_existe.show()
+
+    def get_cedula(self, cedula):
+
+    	usuario = dict()
+    	#Se crea conexión a db
+        self.db = MySQLdb.connect("localhost","dc_ce","dc_ce","dc_ce")
+        self.cursor = self.db.cursor()
+        #Se verifica que no existe la cedula
+        ver_cedula = ('''SELECT idusuarios, nombre, apellido, clave, activo FROM usuarios WHERE cedula = %s''')                                 
+        self.cursor.execute(ver_cedula, cedula)
+        row = self.cursor.fetchone()
+
+        if row is not None:        	
+
+        	usuario["idusuarios"] = row[0]
+        	usuario["nombre"] = row[1]
+        	usuario["apellido"] = row[2]
+        	usuario["clave"] = row[3]
+        	usuario["activo"] = row[4]
+        	usuario["cedula"] = cedula
+
+        #cerrar conexion			
+        self.cursor.close()
+        self.db.close()
+
+        return usuario
+
+    def set_modificar_usuario(self, usuario):
+    	print "modificar_usuario"    	
+    	self.entry_modificar_nombre.set_text(usuario["nombre"])
+        self.entry_modificar_apellido.set_text(usuario["apellido"])
+        self.entry_modificar_cedula.set_text(usuario["cedula"])
+        self.entry_modificar_clave.set_text(usuario["clave"])
+
+        self.idusuarios = usuario["idusuarios"]
+
+        if usuario["activo"] == "N":
+        	index = 0
+        else:
+        	if usuario["activo"] == "S":
+        		index = 1
+
+        self.combobox_activar_modificar_usuario.set_active(index)
+
+    	self.window_modificar_usuario.show()
+
+    def on_boton_aceptar_usuario_modificar_clicked(self, widget):
+		print "on_boton_aceptar_usuario_modificar_clicked"
+		errores = self.validar_datos_usuario_modifcar()
+
+		if len(errores) > 0:
+			self.window_errores_incluir_usuario.show()
+			errores_text = ""
+			for error in errores:
+				errores_text =  errores_text + error +"\n"
+			self.label_errores_insertar_usuario.set_text(errores_text)
+		else:
+			print "modificar"
+			self.modificar_usuario()
+
+
+    def validar_datos_usuario_modifcar(self):
+
+    	errores = []
+
+    	self.nombre = self.entry_modificar_nombre.get_text()
+        self.apellido = self.entry_modificar_apellido.get_text()
+        self.cedula =self.entry_modificar_cedula.get_text()
+        self.clave = self.entry_modificar_clave.get_text()
+        self.activo = self.item
+
+        if self.nombre == "":
+        	errores.append("El nombre no puede ser vacio")
+        if self.apellido == "":
+        	errores.append("El apellido no puede ser vacio")
+        if self.cedula == "":
+        	errores.append("La cédula no puede ser vacia")
+        else:
+        	if not self.cedula.isdigit():
+        		errores.append("La cédula debe ser numérica")
+        if self.clave == "":
+        	errores.append("La clave no puede ser vacia")
+
+    	return errores
+
+    def modificar_usuario(self):
+    	#Se crea conexión a db
+        self.db = MySQLdb.connect("localhost","dc_ce","dc_ce","dc_ce")
+
+        if self.activo == "No":
+        	act = "N"
+        else:
+        	if self.activo == "Si":
+        		act = "S"
+
+        try:
+            self.cursor = self.db.cursor()
+	         
+	        #Se actualiza un registro en la tabla usuarios
+            upd_usuario = ('''UPDATE usuarios 
+            	SET nombre=%s, apellido=%s, cedula=%s, clave=%s, activo=%s 
+            	WHERE idusuarios=%s;''')
+
+            args = (self.nombre, self.apellido, self.cedula, self.clave, act, self.idusuarios)
+            self.cursor.execute (upd_usuario, args)
+            self.db.commit()
+            print "usuario modificado"
+            self.window_usuario_modificado.show()
+        except:
+            print("Error inesperado:", sys.exc_info())
+            self.db.rollback()
+        #cerrar conexion			
+        self.cursor.close()
+        self.db.close()
+
+    def on_button_usuario_modificado_clicked(self, widget):
+        self.window_usuario_modificado.hide()
+        self.window_modificar_usuario.hide()
+    	
 
 if __name__ == '__main__':
     p = Proceso_usuarios() 
